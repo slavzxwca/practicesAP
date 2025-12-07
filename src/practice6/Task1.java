dd1a5f5ceffe1714b1314e5e826d3b7e864b1d02
slavzxwca <ghostslavik2@gmail.com>
2025-12-07 03:42:02 +0200

    Practice 6 added

---
+++ src/practice6/Task1.java
@@ -0,0 +1,31 @@
+package practice6;
+
+import java.util.*;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rnd = new Random();
+
+        System.out.print("Введите размер массива: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.print("Массив: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = rnd.nextInt(100);
+            System.out.print(arr[i] + " ");
+        }
+
+        int even = 0, odd = 0;
+
+        for (int x : arr) {
+            if (x % 2 == 0) even++;
+            else odd++;
+        }
+
+        System.out.println("\nЧётных: " + even);
+        System.out.println("Нечётных: " + odd);
+    }
+}
---
+++ src/practice6/Task4.java
@@ -0,0 +1,23 @@
+package practice6;
+
+public class Task4 {
+    public static void main(String[] args) {
+
+        System.out.println("Таблица синусов 0–90:");
+
+        int count = 0;
+
+        for (int i = 0; i <= 90; i++) {
+            double rad = Math.toRadians(i);
+            double sinValue = Math.sin(rad);
+
+            System.out.printf("%.5f\t", sinValue);
+            count++;
+
+            if (count == 10) {
+                System.out.println();
+                count = 0;
+            }
+        }
+    }
+}
